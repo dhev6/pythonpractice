858a30e72779ed1e7e0cbca83ceb4a4db7a9adfe
chandruAdobe <tec71502@adobe.com>
2025-12-11 18:45:08 +0530

    adding validation in calculator

--- calculator.py
+++ calculator.py
@@ -1,31 +1,33 @@
+git add .
+git commit -m "adding validation in calculator"
+git push origin main
+
 import math
 
 print("welcome to simple calculator")
 
 def choice():
-    number_1 = input("enter first number:")
-    number_2 = input("enter second number:")
-    choice = input("enter operator (+,-,*,/):")
-    if choice == "+":
-        choice = int(y) + int(z)
+    while True:
+        try:
+            number_1 = int(input("choose a first number between 1 to 8:"))
+            number_2 = int(input("choose a second number between 1 to 8:"))
+            choice = input("enter operator (+,-,*,/):")
+            if number_1 in range(1,8) or number_2 in range(1,8) or choice in ['+','-','*','/']:   
+                break
+            else:
+                print("invalid number, please try again")
+        except ValueError:
+            print("invalid number, please try again")
+   
+    if choice == "+": 
+        choice = int(number_1) + int(number_2)
     if choice == "-":
-        choice = int(y) - int(z)
+        choice = int(number_1) - int(number_2)
     if choice == "*":
-        choice = int(y) * int(z)
+        choice = int(number_1) * int(number_2)
     if choice == "/":
-        choice = int(y) / int(z)
+        choice = int(number_1) / int(number_2)
 
-print(f"the result is {choice}")
-
-while True:
-    try:
-        y = input("enter first number:")
-        z = input("enter second number:")
-        choice = input("enter operator (+,-,*,/):")
-        if choice in ['+','-','*','/']:
-            break
-        else:
-            print("invalid input, please try again")
-    except ValueError:
-        print("invalid option, please try again")
     print(f"the result is {choice}")
+
+choice()
